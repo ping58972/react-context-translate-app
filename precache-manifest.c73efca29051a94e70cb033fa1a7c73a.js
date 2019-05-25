9de2d3510c062a5cf0c480bddb1cf69c5236e6f4
ping58972 <ping58972@gmail.com>
2019-05-25 01:30:09 -0500

    Updates

---
+++ precache-manifest.c73efca29051a94e70cb033fa1a7c73a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633f4ae81df6da20e34ceebcab03cd0d",
+    "url": "/react-context-translate-app/index.html"
+  },
+  {
+    "revision": "efe34edcc5247de89a8b",
+    "url": "/react-context-translate-app/static/js/2.dc155f86.chunk.js"
+  },
+  {
+    "revision": "ca97e2ddeec51ae335cb",
+    "url": "/react-context-translate-app/static/js/main.87a721cd.chunk.js"
+  },
+  {
+    "revision": "34951a48a55511d99925",
+    "url": "/react-context-translate-app/static/js/runtime~main.653c7cca.js"
+  }
+]);
