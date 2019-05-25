07a0d05a520cd9406c30335b1d0dc82b5d5775d9
ping58972 <ping58972@gmail.com>
2019-05-24 22:25:34 -0500

    start build translate App

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Button extends Component {
+    render() {
+        return (
+            <button className="ui button primary">Submit</button>
+        )
+    }
+}
+
+export default Button;
